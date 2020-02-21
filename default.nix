33c164dc99ab9c991eaf9ef77b698e9ce05d2235
Karl Palmskog <palmskog@gmail.com>
2020-02-21 11:06:56 -0600

    adjustments to meta.yml to fit coq-community conventions, generate boilerplate

---
+++ default.nix
@@ -0,0 +1,35 @@
+{ pkgs ? (import <nixpkgs> {}), coq-version-or-url, shell ? false }:
+
+let
+  coq-version-parts = builtins.match "([0-9]+).([0-9]+)" coq-version-or-url;
+  coqPackages =
+    if coq-version-parts == null then
+      pkgs.mkCoqPackages (import (fetchTarball coq-version-or-url) {})
+    else
+      pkgs."coqPackages_${builtins.concatStringsSep "_" coq-version-parts}";
+in
+
+with coqPackages;
+
+let
+  ssreflect =
+    coqPackages.ssreflect.overrideAttrs(_: rec {
+      name = "coq-${coq.coq-version}-ssreflect-${version}";
+      version = "dev";
+      src = fetchTarball "https://github.com/math-comp/math-comp/tarball/master";
+    });
+in
+
+pkgs.stdenv.mkDerivation {
+
+  name = "reglang";
+
+  propagatedBuildInputs = [
+    coq
+    ssreflect
+  ];
+
+  src = if shell then null else ./.;
+
+  installFlags = "COQLIB=$(out)/lib/coq/${coq.coq-version}/";
+}
