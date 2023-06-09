a327803c6976137b0b6da07db50e01e3676e0e70
Karl Palmskog <palmskog@gmail.com>
2023-06-09 19:57:45 +0200

    add explicit export locality for instances

--- theories/setoid_leq.v
+++ theories/setoid_leq.v
@@ -18,36 +18,36 @@ Inductive leb a b := Leb of (a ==> b).
 Lemma leb_eq a b : leb a b <-> (a -> b).
 Proof. move: a b => [|] [|]; firstorder. Qed.
 
-Instance: PreOrder leb.
+#[export] Instance: PreOrder leb.
 Proof. split => [[|]|[|][|][|][?][?]]; try exact: Leb. Qed.
 
-Instance: Proper (leb ==> leb ==> leb) andb.
+#[export] Instance: Proper (leb ==> leb ==> leb) andb.
 Proof. move => [|] [|] [A] c d [B]; exact: Leb. Qed.
 
-Instance: Proper (leb ==> leb ==> leb) orb.
+#[export] Instance: Proper (leb ==> leb ==> leb) orb.
 Proof. move => [|] [|] [A] [|] d [B]; exact: Leb. Qed.
 
-Instance: Proper (leb ==> impl) is_true.
+#[export] Instance: Proper (leb ==> impl) is_true.
 Proof. move => a b []. exact: implyP. Qed.
 
 (** Instances for le  *)
 
-Instance: Proper (le --> le ++> leb) leq.
+#[export] Instance: Proper (le --> le ++> leb) leq.
 Proof. move => n m /leP ? n' m' /leP ?. apply/leb_eq => ?. eauto using leq_trans. Qed.
 
-Instance: Proper (le ==> le ==> le) addn.
+#[export] Instance: Proper (le ==> le ==> le) addn.
 Proof. move => n m /leP ? n' m' /leP ?. apply/leP. exact: leq_add. Qed.
 
-Instance: Proper (le ==> le ==> le) muln.
+#[export] Instance: Proper (le ==> le ==> le) muln.
 Proof. move => n m /leP ? n' m' /leP ?. apply/leP. exact: leq_mul. Qed.
 
-Instance: Proper (le ++> le --> le) subn.
+#[export] Instance: Proper (le ++> le --> le) subn.
 Proof. move => n m /leP ? n' m' /leP ?. apply/leP. exact: leq_sub. Qed.
 
-Instance: Proper (le ==> le) S.
+#[export] Instance: Proper (le ==> le) S.
 Proof. move => n m /leP ?. apply/leP. by rewrite ltnS. Qed.
 
-Instance: RewriteRelation le := Build_RewriteRelation _.
+#[export] Instance: RewriteRelation le := Build_RewriteRelation _.
 
 (** Wrapper Lemma to trigger setoid rewrite *)
 Definition leqRW m n  : m <= n -> le m n := leP.
